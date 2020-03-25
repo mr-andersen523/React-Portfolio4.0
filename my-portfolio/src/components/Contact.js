0e44cef69dd122a206a9fcc08fc2f4390cbad6d9
mr-andersen523 <chasea523@gmail.com>
2020-03-25 12:11:53 -0700

    added links to icons and style

--- my-portfolio/src/components/Contact.js
+++ my-portfolio/src/components/Contact.js
@@ -12,22 +12,22 @@ class Contact extends Component {
     render() { 
         return (  
         <div>
-        <p style={{color: "gray", fontWeight:"bold", marginLeft:"11em", paddingTop:"9em", fontFamily:'Campton', fontSize:"30px"}}>
+        <p style={{color: "gray", fontWeight:"bold", marginLeft:"11em", textAlign:"left",paddingTop:"5em", fontFamily:'Campton', fontSize:"30px"}}>
                 <Fade bottom cascade> Reach out to me!</Fade>
         </p>
         <Fade bottom>
-        <div>
-            <h1><a href="mailto:chasea523@gmail.com?Subject=Hello!" id="email">chasea523@gmail.com</a></h1>
+        <div style={{ display:'inline-block'}}>
+            <h1 style={{textAlign:"center"}}><a href="mailto:chasea523@gmail.com?Subject=Hello!" id="email">chasea523@gmail.com</a></h1>
                 <br></br>
                 <br></br>
-            <ul style={{ paddingTop:"12em", marginBottom:"2em"}}>
+            <ul style={{ paddingTop:"5em", marginBottom:"2em", textAlign:"center"}}>
                 
-                <li><FontAwesomeIcon icon={['fab', 'github']} size="2x" /> </li>
-                <li><FontAwesomeIcon icon={['fab', 'linkedin']} size="2x"/> </li>
-                <li><FontAwesomeIcon icon={['fab', 'codepen']} size="2x" /> </li>
-                <li><FontAwesomeIcon icon={['fab', 'dribbble']} size="2x" /> </li>
-                <li><FontAwesomeIcon icon={['fab', 'instagram']} size="2x" /> </li>
-                <li><FontAwesomeIcon icon={['fab', 'behance']} size="2x" /> </li>
+                <li><a href='https://github.com/mr-andersen523'><FontAwesomeIcon icon={['fab', 'github']} size="2x" /> </a></li>
+                <li><a href='https://www.linkedin.com/in/chase-andersen-26275290/'><FontAwesomeIcon icon={['fab', 'linkedin']} size="2x"/> </a></li>
+                <li><a href='https://codepen.io/mr-andersen523'><FontAwesomeIcon icon={['fab', 'codepen']} size="2x" /> </a></li>
+                <li><a href='https://dribbble.com/mr-andersen523'><FontAwesomeIcon icon={['fab', 'dribbble']} size="2x" /> </a></li>
+                <li><a href='https://www.instagram.com/mr_andersen523/'><FontAwesomeIcon icon={['fab', 'instagram']} size="2x" /> </a></li>
+                <li><a href='https://www.behance.net/chaseandersen'><FontAwesomeIcon icon={['fab', 'behance']} size="2x" /> </a></li>
             </ul>
         </div>
         </Fade>
