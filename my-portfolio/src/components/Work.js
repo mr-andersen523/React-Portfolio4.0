519ac8bba26e3204e4b68d1cedeb7af898cadb89
mr-andersen523 <chasea523@gmail.com>
2020-03-25 02:02:11 -0700

    template for portfolio work

---
+++ my-portfolio/src/components/Work.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import Fade from 'react-reveal/Fade';
+
+
+
+
+
+class Work extends Component {
+    render() { 
+        return (
+        <div id="work">
+            <h1 className='heading'>
+                <Fade bottom cascade> Work.</Fade>
+            </h1>
+
+        </div>
+        );
+    }
+}
+
+export default Work;
